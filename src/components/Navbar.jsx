f046acef9c5ac122eb86157f0308c3e5d877c291
El Mostafa <2007013000103@ofppt-edu.ma>
2026-04-06 11:11:48 +0100

    add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,38 @@ import React from "react";
 import "./css/Navbar.css";
 import { Link } from "react-router-dom";
 
-export default function Navbar(){
+export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/">FreelanceHub</Link>
+        <Link to="/">
+          <img src="/img/logon.png" style={{ height: "190px", width: "auto" }} alt="Jobsy" />
+        </Link>
       </div>
 
       <ul className="navbar-links">
-        <li><Link to="/client/dashboard">Clients</Link></li>
-        <li><Link to="/freelancer/browse-projects">Freelancers</Link></li>
-        <li><Link to="/shared/project-details">Projets</Link></li>
-        <li><Link to="/shared/chat">Messages</Link></li>
+        <li>
+          <Link to="/client/dashboard">Clients</Link>
+        </li>
+        <li>
+          <Link to="/freelancer/browse-projects">Freelancers</Link>
+        </li>
+        <li>
+          <Link to="/shared/project-details">Projets</Link>
+        </li>
+        <li>
+          <Link to="/shared/chat">Messages</Link>
+        </li>
       </ul>
 
       <div className="navbar-auth">
-        <Link to="/auth/login" className="btn-secondary">Connexion</Link>
-        <Link to="/auth/register" className="btn-primary">Inscription</Link>
+        <Link to="/auth/login" className="btn-secondary">
+          Connexion
+        </Link>
+        <Link to="/auth/register" className="btn-primary">
+          Inscription
+        </Link>
       </div>
     </nav>
   );
-};
+}
