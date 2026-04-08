bba3fe6206bb1fb1f44cac66091a8b0bf15f57ff
El Mostafa Belayd <2007013000103@ofppt-edu.ma>
2026-04-08 11:19:28 +0100

    modifit structure

---
+++ src/pages/routes/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+
+import { Route, Routes } from "react-router-dom";
+
+
+export default function ProtectedRoute(){
+  return(
+      <div>
+        
+      </div>
+  );
+}
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+export const store = configureStore({
+    // reducer: {
+    //     auth: authReducer,
+    //     project: projectReducer,
+    //     proposal: proposalReducer,
+    //     message: messageReducer,
+    //     review: reviewReducer,
+    //     profile: profileReducer,
+    //     skill: skillReducer,
+    // },
+});
